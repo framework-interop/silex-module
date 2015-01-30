dc6f622e24f54a5ac68f1fb9e31ebb2e2b7e80d5
David Négrier <d.negrier@thecodingmachine.com>
2015-01-30 10:31:28 +0100

    Initial commit

---
+++ README.md
@@ -0,0 +1,14 @@
+Silex module for framework-interop
+==================================
+
+This package contains a *framework-interop* module that provides a Silex application.
+
+It can be used by other framework-agnostic modules to ensure that a Silex application
+is present and can be used.
+
+##Framework-agnostic modules?
+
+Have a look at [the demo](http://github.com/framework-interop/framework-interop-demo).
+It shows 3 modules, one using Symfony 2, one using Silex and one using Zend Framework 1.
+
+## How it works
---
+++ src/SilexModule.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace Interop\Framework\Silex;
+
+use Interop\Framework\Module;
+use Interop\Framework\ModuleInterface;
+use Interop\Container\ContainerInterface;
+use Mouf\StackPhp\SilexMiddleware;
+use Interop\Framework\HttpModuleInterface;
+use Symfony\Component\HttpKernel\HttpKernelInterface;
+
+/**
+ * This module provides a base Silex application.
+ * Other modules can hook on that application.
+ */
+class SilexModule implements HttpModuleInterface
+{
+    private $rootContainer;
+		private $silex;
+
+    public function getName()
+    {
+        return 'silex';
+    }
+
+    public function getContainer(ContainerInterface $rootContainer)
+    {
+    	$this->rootContainer = $rootContainer;
+			$this->silex = new Application($this->rootContainer);
+
+			// Let's put the silex app in the container... that is itself the silex app :)
+			$this->silex['silexApp'] = $this->silex;
+
+			// The app is the container.
+      return $this->silex;
+    }
+
+	/* (non-PHPdoc)
+	 * @see \Interop\Framework\ModuleInterface::init()
+	 */
+	public function init() {
+
+	}
+
+	/* (non-PHPdoc)
+	 * @see \Interop\Framework\HttpModuleInterface::getHttpMiddleware()
+	 */
+	public function getHttpMiddleware(HttpKernelInterface $app) {
+
+		return new SilexMiddleware($app, $this->silex);
+	}
+
+}
